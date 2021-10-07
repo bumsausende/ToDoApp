3818753fe14a6affba7e38440f16769c2b54d8aa
bumsausende <feinen.kerl@gmail.com>
2021-10-07 12:31:25 +0200

    it works with local storage yeah"

--- src/Components/ToDoList.jsx
+++ src/Components/ToDoList.jsx
@@ -1,14 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ToDoItem } from "./ToDoItem";
 import { CreateToDo } from "./CreateToDo";
+import { getItem, setItem } from "../utils/localStorage";
 
+// now with getItems
 export const ToDoList = () => {
-  const [ToDos, setToDos] = useState([
-    { id: 0, name: "klarkommen" },
-    { id: 1, name: "machen" },
-    { id: 2, name: "Abwasch stehen lassen" },
-  ]);
-  console.log(ToDos);
+  const [ToDos, setToDos] = useState(
+    getItem("ToDos", [
+      { id: 0, name: "klarkommen" },
+      { id: 1, name: "machen" },
+      { id: 2, name: "Abwasch stehen lassen" },
+    ])
+  );
+  //setItem useEFFECT
+
+  useEffect(() => {
+    setItem("ToDos", ToDos);
+  }, [ToDos]);
 
   return (
     <>
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import { App } from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { App } from "./App";
 /*import { ToDoList } from "./Components/ToDoList"
 import { ToDoItem } from "./Components/ToDoItem"*/
 
 ReactDOM.render(
   <React.StrictMode>
-  < App /></React.StrictMode>, 
-  document.getElementById('root')
-)
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
